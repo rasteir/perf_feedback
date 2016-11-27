8e8165e31fd5d46d77e5e54e23cc41f04f36fdb2
rasteir <rasteir@gmail.com>
2016-11-27 12:55:40 +0100

    added read page

--- js/insert.js
+++ js/insert.js
@@ -7,14 +7,14 @@ $(document).ready(function () {
 			success: function (msg) {
 				if (msg == 1) {
 					$("#res").html("<h4 class='bg-success top-gap gap-bottom'>Your feedback for " + $("#name").val() + " has been saved</h4>");
-					window.location.href = "#startp	";
+					window.location.href = "#startp";
 					document.getElementById("name").value = "";
 					document.getElementById("strenghts-comments").value = "";
 					document.getElementById("improvement-comments").value = "";
 					document.getElementById("free-comments").value = "";
 				}
 				else {
-					window.location.href = "#startp	";
+					window.location.href = "#startp";
 					$("#res").html("<h4 class='bg-danger top-gap gap-bottom'>An error occured when submitting your feedback</h4>");
 				}
 			}
