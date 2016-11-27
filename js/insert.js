a548256d142506cda6bc6bdba5a8997840310f7f
rasteir <rasteir@gmail.com>
2016-11-27 12:35:34 +0100

    log submit

--- js/insert.js
+++ js/insert.js
@@ -3,18 +3,19 @@ $(document).ready(function () {
 		$.ajax({
 			type: "POST",
 			url: "php/insert.php",
-			data: "name=" + $("#name").val() + "&strenghts-comments=" + $("#strenghts-comments").val() + "&improvement-comments=" + $("#improvement-comments").val() + "&free-comments=" + $("#free-comments").val(),
+			data: "name=" + ($("#name").val()).toLowerCase() + "&strenghts-comments=" + $("#strenghts-comments").val() + "&improvement-comments=" + $("#improvement-comments").val() + "&free-comments=" + $("#free-comments").val(),
 			success: function (msg) {
 				if (msg == 1) {
-					$("#res").html("<p>Your feedback for " + $("#name").val() + " has been saved</p>");
-					window.location.href = "index.html"
+					$("#res").html("<h4 class='bg-success top-gap gap-bottom'>Your feedback for " + $("#name").val() + " has been saved</h4>");
+					window.location.href = "#startp	";
 					document.getElementById("name").value = "";
 					document.getElementById("strenghts-comments").value = "";
 					document.getElementById("improvement-comments").value = "";
 					document.getElementById("free-comments").value = "";
 				}
 				else {
-					$("#res").html("<p>An error occured when submitting your feedback</p>");
+					window.location.href = "#startp	";
+					$("#res").html("<h4 class='bg-danger top-gap gap-bottom'>An error occured when submitting your feedback</h4>");
 				}
 			}
 		});
